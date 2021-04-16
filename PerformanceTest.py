46116a791bc64fc6a433826a5fed712047dc040e
Bruno Moreira <bmoreira@cignifi.com>
2021-04-16 10:57:56 -0300

    primeira versão

---
+++ PerformanceTest.py
@@ -0,0 +1,47 @@
+
+import requests
+import datetime
+
+
+
+session = requests.Session()
+
+def singleHttpRequests():
+
+    print("Starting SingleHttpClient")
+    totalValue = 0;
+    median = 0.0;
+    for item in range(10):
+        before = datetime.datetime.now()
+        r = requests.get("https://api.oiads.com.br/score/address/v1")
+        after = datetime.datetime.now()
+        span = after - before
+        print(f"{r.status_code}  -  {span.microseconds / 1000}") 
+        totalValue += span.microseconds
+        median = totalValue / (item + 1)
+
+    print("Connections done")
+    print(f"total time: {totalValue / 1000}")
+    print(f"Average: {median / 1000}")
+
+def multipleHttpRequests():
+    
+    print("Starting MultipleHttpClient")
+    totalValue = 0;
+    median = 0.0;
+    
+    for item in range(10):
+        before = datetime.datetime.now()
+        r = session.get("https://api.oiads.com.br/score/address/v1")
+        after = datetime.datetime.now()
+        span = after - before
+        print(f"{r.status_code}  -  {span.microseconds / 1000}") 
+        totalValue += span.microseconds
+        median = totalValue / (item + 1)
+
+    print("Connections done")
+    print(f"total time: {totalValue / 1000}")
+    print(f"Average: {median / 1000}")
+
+singleHttpRequests()
+multipleHttpRequests()
